285759d61ed033a66eba230b4b9197b596f795f9
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-31 13:07:56 -0700

    setting up router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,6 +11,14 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Route path="/how-it-works" component={} />
+      <Route path="/add-pet-profile" component={} />
+      <Route path="/your-pets/medical-record/:id" component={} />
+      <Route path="/your-pets/daily-log/:id" component={} />
+      <Route path="/your-pets/appointments/:id" component={} />
+      <Route path="/your-pets" component={} />
+      <Route path="/" component={} />
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
