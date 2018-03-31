5ee4709c49963077369b7ebab8e7fde0edb08fd8
Marc Cooperstein <mcooperstein@gmail.com>
2018-03-31 16:21:07 -0700

    sign out button link to homepage

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,13 +1,18 @@
-import React from 'react';
-
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import { auth } from '../firebase';
 
-const SignOutButton = () =>
-  <button
-    type="button"
-    onClick={auth.doSignOut}
-  >
-    Sign Out
-  </button>
+class SignOutButton extends Component{
+  render(){
+    return(
+      <div>
+        <button type="button" onClick={auth.doSignOut}>
+          <Link to="/">Sign Out</Link>
+        </button>
+      </div>
+    )
+  }
+}
+
 
 export default SignOutButton;
