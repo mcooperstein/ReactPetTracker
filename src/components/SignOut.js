0c00255bac5a6ced4415d213fdacc67c79c1b2aa
maxadorable <area51alienware@gmail.com>
2018-04-04 12:30:20 -0700

    Fixed the signout

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -3,9 +3,13 @@ import {Link} from 'react-router-dom';
 import { auth } from '../firebase';
 
 class SignOutButton extends Component{
+  refresh()
+  {
+    setTimeout(location.reload(), 1000);
+  }
   render(){
     return(
-      <div>
+      <div onClick={ this.refresh }>
         <button className="btn btn-warning" onClick={auth.doSignOut}>
           {/* <button type="button" onClick={this.setState(() => ({ authUser: null }))}> */}
           {/* <Link to="/">Sign Out</Link> */}
